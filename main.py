8214fc4196f8fb102bb5bc30af61ee160202fc0d
Kuru Juhani <juhani.kuru@lapinamk.fi>
2025-04-10 10:16:05 +0300

    add cors

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import  FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import controllers.restaurant_ratings
 
+
 app = FastAPI()
+
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 app.include_router(controllers.restaurant_ratings.router)
